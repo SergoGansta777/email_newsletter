6904a7aa33be156f9daf75422f2d8c4a9b70c097
Сергей Нехорошев <sergejnehorosev@Sergejs-MacBook-Pro.local>
2024-07-21 22:18:27 +0300

    Add email validation via crate, update unit tests

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,10 +1,15 @@
+use validator::ValidateEmail;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(s: String) -> Result<SubscriberEmail, String> {
-        // TODO: add validation!
-        Ok(Self(s))
+        if ValidateEmail::validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
     }
 }
 
@@ -18,16 +23,27 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+        claims::assert_ok!(SubscriberEmail::parse(email));
+    }
+
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
     #[test]
     fn email_missing_at_symbol_is_rejected() {
         let email = "ursuladomain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
+
     #[test]
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
